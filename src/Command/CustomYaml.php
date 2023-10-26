56dacfb8a339745bc1819e5917ae6e0c5d32d7af
Patrik Freij <patrik.t.freij@gmail.com>
2023-10-26 20:07:48 +0000

    Add custom-yaml command

---
+++ src/Command/CustomYaml.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace loyen\DndbCharacterSheet\Command;
+
+use Composer\Script\Event;
+use loyen\DndbCharacterSheet\Importer\CustomYaml\CustomYamlImporter;
+use loyen\DndbCharacterSheet\Importer\ImporterException;
+use loyen\DndbCharacterSheet\Sheet;
+
+class CustomYaml
+{
+    public static function fromFile(Event $event): void
+    {
+        $exitCode = 0;
+
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        require_once $vendorDir . '/autoload.php';
+
+        $arguments = $event->getArguments();
+        $filePath = filter_var(array_pop($arguments), \FILTER_SANITIZE_STRING, \FILTER_NULL_ON_FAILURE);
+
+        if (!$filePath || !file_exists($filePath)) {
+            throw new ImporterException('No file inputted.');
+        }
+
+        $fileContent = file_get_contents($filePath)
+            ?: throw new ImporterException('Failed to read inputted file.');
+        $character = CustomYamlImporter::import($fileContent);
+
+        if (\in_array('--json', $arguments, true)) {
+            echo json_encode(
+                $character,
+                \JSON_PRETTY_PRINT
+            );
+        } else {
+            $sheet = new Sheet();
+            echo $sheet->render($character);
+        }
+
+        echo \PHP_EOL;
+        exit($exitCode);
+    }
+}
